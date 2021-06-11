8d731dd5cd789ad74601d0f9ab1163fc2974fbbe
glilley54 <glilley54@gmail.com>
2021-06-11 16:29:23 +0100

    palane class setup complete

--- src/main/java/airline/Plane.java
+++ src/main/java/airline/Plane.java
@@ -1,4 +1,38 @@
 package airline;
 
 public class Plane {
+
+    private String type;
+    private int capacity;
+    private int totalWeight;
+
+    public Plane(String type, int capacity, int totalWeight) {
+        this.type = type;
+        this.capacity = capacity;
+        this.totalWeight = totalWeight;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getCapcity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public int getTotalWeight() {
+        return totalWeight;
+    }
+
+    public void setTotalWeight(int totalWeight) {
+        this.totalWeight = totalWeight;
+    }
 }
