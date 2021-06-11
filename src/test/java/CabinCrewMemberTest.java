8df8ada7bbb57955583e10da4ed7311731541188
glilley54 <glilley54@gmail.com>
2021-06-11 15:22:56 +0100

    Cabin crew test 2 test pass

--- src/test/java/CabinCrewMemberTest.java
+++ src/test/java/CabinCrewMemberTest.java
@@ -1,12 +1,25 @@
 import org.junit.Before;
+import org.junit.Test;
 import people.CabinCrewMember;
+
+import static org.junit.Assert.assertEquals;
 
 public class CabinCrewMemberTest {
 
     private CabinCrewMember cabinCrewMember;
 
     @Before
-    public void before(){
-        cabinCrewMember = new CabinCrewMember("Jen","manager");
+    public void before() {
+        cabinCrewMember = new CabinCrewMember("Jen", "cabin crew manager");
+    }
+
+    @Test
+    public void hasName() {
+        assertEquals("Jen", cabinCrewMember.getName());
+    }
+
+    @Test
+    public void hasRank() {
+        assertEquals("cabin crew manager", cabinCrewMember.getRank());
     }
 }
