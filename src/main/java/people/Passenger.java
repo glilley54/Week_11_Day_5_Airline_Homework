3de24b2c258daf13fb71f72bc0ef8964f38cd973
glilley54 <glilley54@gmail.com>
2021-06-11 14:35:47 +0100

    Pilot Class set up

--- src/main/java/people/Passenger.java
+++ src/main/java/people/Passenger.java
@@ -1,4 +1,22 @@
 package people;
 
-public class Passenger {
+
+
+public class Passenger extends Person{
+
+    private int numberOfBags;
+
+    public Passenger(String name,int numberOfBags) {
+        super(name);
+        this.numberofBags = numberOfBags;
+
+    }
+
+    public int getNumberOfBags() {
+        return numberOfBags;
+    }
+
+    public void setNumberOfBags(int numberOfBags) {
+        this.numberOfBags = numberOfBags;
+    }
 }
--- src/main/java/people/Pilot.java
+++ src/main/java/people/Pilot.java
@@ -1,4 +1,31 @@
 package people;
 
-public class Pilot {
+import javax.swing.*;
+
+public class Pilot extends Person {
+
+    private String rank;
+    private String licenceNumber;
+
+    public Pilot(String name, String rank) {
+        super(name);
+        this.rank = rank;
+        this.licenceNumber = licenceNumber;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+
+    public String getLicenceNumber() {
+        return licenceNumber;
+    }
+
+    public void setLicenceNumber(String licenceNumber) {
+        this.licenceNumber = licenceNumber;
+    }
 }
